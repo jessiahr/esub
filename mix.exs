8d25eada1d1cb276b5776adec9788b4d777accfb
Jessiah Ratliff <jessiahr@Jessiahs-MacBook-Air.local>
2017-12-21 13:34:36 +0800

    add license

--- mix.exs
+++ mix.exs
@@ -7,6 +7,9 @@ defmodule Esub.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      name: "Esub",
       deps: deps()
     ]
   end
@@ -19,11 +22,24 @@ defmodule Esub.Mixfile do
     ]
   end
 
+  defp description do
+    """
+    A simple event subscription framework. 
+    """
+  end
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Jessiah Ratliff"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/jessiahr/esub"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.18", only: :dev}
     ]
   end
 end
